2354ed0df8a4ebbfa360d6c3083d48398488823c
Toka Abdulhamied <34253897+TokaAbdulhamied@users.noreply.github.com>
2023-06-06 16:10:54 +0300

    Time: 85 ms (68.40%), Space: 44 MB (99.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,20 +3,23 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let hashWindow = {}; 
+    let hashWindow = new Map(); 
     let windowStart = 0, windowLength =0 , longest=0; 
     let i ; 
     for ( i =0 ; i < s.length ; i++) {
         let char = s[i]; 
-        if (hashWindow[char] != undefined){ 
-            if (hashWindow[char] >= windowStart) {
+        if (!hashWindow.has(char)) {
+            hashWindow.set(char, i)  
+        }
+        else { 
+            if (hashWindow.get(char) >= windowStart) {
                 windowLength = i- windowStart ;
                 if (longest < windowLength) longest = windowLength;
-                windowStart = hashWindow[char] +1; 
+                windowStart = hashWindow.get(char) +1; 
             }
+            hashWindow.set(char, i)  
         }
-      hashWindow[char] = i;  
-
+        
     }
     if (longest < i - windowStart)
         longest = i - windowStart;
