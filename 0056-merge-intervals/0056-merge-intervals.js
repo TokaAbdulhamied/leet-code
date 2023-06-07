4a3a84f8831134f537b896b9d8254f04ed3907fa
Toka Abdulhamied <34253897+TokaAbdulhamied@users.noreply.github.com>
2023-06-07 15:08:20 +0300

    Time: 133 ms (11.88%), Space: 51.8 MB (11.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -4,17 +4,19 @@
  */
 var merge = function(intervals) {
     let result = []; 
-    intervals.sort((a,b)=>a[0] - b[0])
+    intervals.sort((a,b) => a[0] - b[0])
     let currentInterval = intervals[0]; 
 
     console.log (intervals)
     for (let i = 1; i< intervals.length ; i++) {
-        if (intervals[i][0] > currentInterval[1]) {
-            result.push(currentInterval); 
-            currentInterval = intervals[i]
+        const ele = intervals[i]
+        if (currentInterval[1] >= ele[0] ) {
+            currentInterval = [currentInterval[0], Math.max(ele[1],currentInterval[1])]
+
         }
         else {
-            currentInterval = [currentInterval[0], Math.max(intervals[i][1],currentInterval[1] )]
+            result.push(currentInterval); 
+            currentInterval = ele; 
         }
         
     }
